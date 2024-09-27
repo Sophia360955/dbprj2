ae4752e4891629e2f62ace29432045376f928fba
unknown <sophiadd73@gmail.com>
2024-09-27 23:28:44 +0630

    change connection2

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-# streamlit_app.py
-
+import pymysql
+import pandas as pd
 import streamlit as st
-# Initialize connection.
-conn = st.connection('mysql', type='sql')
 
-# Perform query.
-df = conn.query('SELECT * from mytable;', ttl=600)
+# Function to establish a connection to the MySQL database
+def get_connection():
+    return pymysql.connect(
+        host=st.secrets["mysql"]["host"],
+        user=st.secrets["mysql"]["username"],
+        password=st.secrets["mysql"]["password"],
+        database=st.secrets["mysql"]["database"],
+        port=st.secrets["mysql"]["port"]
+    )
 
-st.write(df)
+# Function to execute a query
+def run_query(query):
+    conn = get_connection()
+    df = pd.read_sql(query, conn)
+    conn.close()
+    return df
+
+# Streamlit App UI
+st.title("MySQL Data Viewer")
+
+if st.button("Load Data"):
+    query = "SELECT * FROM bookings;"  # Change the query as per your table
+    data = run_query(query)
+    st.write(data)
